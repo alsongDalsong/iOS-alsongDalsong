9f2f652c4eacdae5b004a2db4e16fa6d7bd1c231
In Ye, Kim <ide7270@naver.com>
2025-03-10 23:36:41 +0900

    [style] 코드 줄바꿈 처리 및 속성추가

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/AppDelegate.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(
+        _: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options _: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     

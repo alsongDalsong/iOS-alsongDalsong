375759a704311a070e49af079416ddd93d7204bd
In Ye, Kim <ide7270@naver.com>
2025-04-02 18:16:18 +0900

    [feat] 카드 뷰 추가

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Components/SwiftUIComponents/ModeView.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Components/SwiftUIComponents/ModeView.swift
@@ -43,39 +43,72 @@ struct ModeView: View {
     }
 }
 
-extension ModeView {
-    private var frontOverlay: some View {
-          ZStack {
-              VStack(alignment: .leading) {
-                  HStack {
-                      Image(systemName: "clock")
-                      Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.recommended.time))
-                          .font(.doHyeon(size: 20))
-                  }
-
-                  HStack {
-                      Image(systemName: "person.fill")
-                      Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.recommended.peopleCount))
-                          .font(.doHyeon(size: 20))
-                  }
-              }
-              .padding(.top, 16)
-              .padding(.trailing, 24)
-              .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-
-              VStack(alignment: .leading) {
-                  Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.title))
-                      .font(.doHyeon(size: 40))
-                      .layoutPriority(1)
-
-                  Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.summary))
-                      .font(.doHyeon(size: 24))
-                      .minimumScaleFactor(0.01)
-              }
-              .padding(.bottom, 25)
-              .padding(.leading, 24)
-              .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
-          }
-          .foregroundStyle(.white)  //TO-DO
-      }
+private extension ModeView {
+    var cardBaseView: some View {
+        Image(viewModel.selectedCard.mode.imageName)
+            .resizable()
+            .scaledToFit()
+            .blur(radius: viewModel.selectedCard.isFaceUp ? 0 : 10)
+            .cornerRadius(30)
+            .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: 4)
+    }
+    
+    var frontCard: some View {
+        cardBaseView
+            .overlay(frontOverlay)
+    }
+    
+    var backCard: some View {
+        cardBaseView
+            .overlay(backOverlay)
+    }
+    
+    var frontOverlay: some View {
+        ZStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "clock")
+                    Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.recommended.time))
+                        .font(.doHyeon(size: 20))
+                }
+                
+                HStack {
+                    Image(systemName: "person.fill")
+                    Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.recommended.peopleCount))
+                        .font(.doHyeon(size: 20))
+                }
+            }
+            .padding(.top, 16)
+            .padding(.trailing, 24)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            
+            VStack(alignment: .leading) {
+                Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.title))
+                    .font(.doHyeon(size: 40))
+                    .layoutPriority(1)
+                
+                Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.summary))
+                    .font(.doHyeon(size: 24))
+                    .minimumScaleFactor(0.01)
+            }
+            .padding(.bottom, 25)
+            .padding(.leading, 24)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+        }
+        .foregroundStyle(.white)  //TO-DO
+    }
+    
+    var backOverlay: some View {
+        RoundedRectangle(cornerRadius: 30)
+            .fill(Color.white.opacity(0.05))
+            .overlay(
+                VStack(alignment: .leading, spacing: 15) {
+                    Text(viewModel.selectedCard.mode.title)
+                        .font(.doHyeon(size: 40))
+                    Text(LocalizedStringResource(stringLiteral: viewModel.selectedCard.mode.description))
+                        .font(.system(size: 18))
+                }
+                    .padding(.horizontal, 20)
+            )
+    }
 }

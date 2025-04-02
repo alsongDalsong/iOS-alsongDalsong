7429cda7d7eb04838673aee7545bcf3efa25f29f
moral-life <leeminam32@gmail.com>
2025-04-02 16:01:40 +0900

    [feat] 버튼 컨피규레이션, 버튼 그림자 요소 추가, 디자인 변경

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Components/UIKitComponents/ASButtonConfiguration.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Components/UIKitComponents/ASButtonConfiguration.swift
@@ -4,37 +4,48 @@ extension ASButton {
     /// ASButton의 스타일 및 설정 정보를 관리하는 구조체
     struct ASButtonConfiguration {
         let systemImageName: String?
+        let imageSize: CGFloat
         let text: String?
         let textStyle: UIFont.TextStyle
         let backgroundColor: UIColor?
         let cornerStyle: UIButton.Configuration.CornerStyle
         let baseForegroundColor: UIColor
+        let strokeColor: UIColor?
+        let strokeWidth: CGFloat
         init(
             systemImageName: String? = nil,
+            imageSize: CGFloat = 20,
             text: String? = nil,
             textStyle: UIFont.TextStyle = .largeTitle,
             backgroundColor: UIColor? = nil,
             cornerStyle: UIButton.Configuration.CornerStyle = .medium,
-            baseForegroundColor: UIColor = .white
+            baseForegroundColor: UIColor = .white,
+            strokeColor: UIColor? = nil,
+            strokeWidth: CGFloat = 0
         ) {
             self.systemImageName = systemImageName
+            self.imageSize = imageSize
             self.text = text
             self.backgroundColor = backgroundColor
             self.textStyle = textStyle
             self.cornerStyle = cornerStyle
             self.baseForegroundColor = baseForegroundColor
+            self.strokeColor = strokeColor
+            self.strokeWidth = strokeWidth
         }
         
         /// 버튼의 스타일을 만드는 메소드
         func createConfiguration() -> UIButton.Configuration {
             var config = UIButton.Configuration.gray()
             config.baseForegroundColor = baseForegroundColor
+            config.background.strokeColor = strokeColor
+            config.background.strokeWidth = strokeWidth
             
             if let systemImageName {
                 config.imagePlacement = .leading
                 config.image = UIImage(systemName: systemImageName)
-                config.imagePadding = 10
-                let imageConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .heavy)
+                config.imagePadding = 0
+                let imageConfig = UIImage.SymbolConfiguration(pointSize: imageSize, weight: .heavy)
                 config.preferredSymbolConfigurationForImage = imageConfig
             }
             

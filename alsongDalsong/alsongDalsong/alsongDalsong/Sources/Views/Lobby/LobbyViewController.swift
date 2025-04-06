27b4f7a144b588209b3c90fd65c6213b8eaa9327
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2025-04-06 22:18:16 +0900

    [fix] roomNumber Button Text 안보이는 문제 해결

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Lobby/LobbyViewController.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Lobby/LobbyViewController.swift
@@ -58,6 +58,22 @@ final class LobbyViewController: UIViewController {
                 }
             }
             .store(in: &cancellables)
+        
+        viewmodel.$roomNumber
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] roomNumber in
+            self?.roomNumberButton.setConfiguration(
+                text: "#" + roomNumber,
+                textStyle: .largeTitle,
+                backgroundColor: .roomNumberButton,
+                cornerStyle: .large,
+                baseForegroundColor: .asBlack,
+                shadowColor: .backButtonShadow,
+                strokeColor: .backButtonShadow,
+                strokeWidth: 3
+            )
+        }
+        .store(in: &cancellables)
     }
 
     private func setupUI() {

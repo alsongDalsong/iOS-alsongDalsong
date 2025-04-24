b55af7fff5102d364ab2c9e8d594d2a81f905943
around-forest <kleedo19@naver.com>
2025-04-25 02:52:16 +0900

    [feat] CGFloat extension 추가

---
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Extensions/CGFloat+.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+extension CGFloat {
+    @MainActor
+    static func responsiveHeight(_ view: UIView, _ value: CGFloat) -> CGFloat {
+        let baseHeight: CGFloat = 874 // 기준 디바이스 (ex: iPhone 16 Pro)
+        let currentHeight = view.bounds.height
+        return (value / baseHeight) * currentHeight
+    }
+
+    @MainActor
+    static func responsiveWidth(_ view: UIView, _ value: CGFloat) -> CGFloat {
+        let baseWidth: CGFloat = 402
+        let currentWidth = view.bounds.width
+        return (value / baseWidth) * currentWidth
+    }
+}

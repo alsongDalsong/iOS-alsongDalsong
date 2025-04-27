33a17615aec6e92baceea355938e45605520536e
around-forest <kleedo19@naver.com>
2025-04-27 15:42:43 +0900

    [refactor] CGFloat deprecated 코드 수정

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Extensions/CGFloat+.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Extensions/CGFloat+.swift
@@ -4,15 +4,17 @@ import UIKit
 extension CGFloat {
     @MainActor
     static func responsiveHeight(_ value: CGFloat) -> CGFloat {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return 0 }
         let baseHeight: CGFloat = 874 // 기준 디바이스 (ex: iPhone 16 Pro)
-        let currentHeight = UIScreen.main.bounds.height
+        let currentHeight = windowScene.screen.bounds.height
         return (value / baseHeight) * currentHeight
     }
 
     @MainActor
     static func responsiveWidth(_ value: CGFloat) -> CGFloat {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return 0 }
         let baseWidth: CGFloat = 402
-        let currentWidth = UIScreen.main.bounds.width
+        let currentWidth = windowScene.screen.bounds.width
         return (value / baseWidth) * currentWidth
     }
 }

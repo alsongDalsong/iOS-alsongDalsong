6e34b13ad801971db19c7449f4e24637a6ddf686
moral-life <leeminam32@gmail.com>
2025-05-23 03:59:30 +0900

    [fix] Publisher 및 불필요한 코드 제거

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Utils/BgmAudioHelper.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Utils/BgmAudioHelper.swift
@@ -47,43 +47,6 @@ final class BgmAudioHelper: @unchecked Sendable {
 
     private let queue = DispatchQueue(label: "alsongDalsong.AudioHelper")
 
-    // MARK: - Publishers
-
-    private let amplitudeSubject = PassthroughSubject<Float, Never>()
-    private let playerStateSubject = PassthroughSubject<(FileSource, Bool), Never>()
-    private let waveformUpdateSubject = PassthroughSubject<Int, Never>()
-    private let recorderStateSubject = PassthroughSubject<Bool, Never>()
-    private let recorderDataSubject = PassthroughSubject<Data, Never>()
-    private let normalizedFrequencyAmplitudes = PassthroughSubject<[Float], Never>()
-    private let playerEnginePrgress = PassthroughSubject<Double, Never>()
-    var amplitudePublisher: AnyPublisher<Float, Never> {
-        amplitudeSubject.eraseToAnyPublisher()
-    }
-
-    var playerStatePublisher: AnyPublisher<(FileSource, Bool), Never> {
-        playerStateSubject.eraseToAnyPublisher()
-    }
-
-    var waveformUpdatePublisher: AnyPublisher<Int, Never> {
-        waveformUpdateSubject.eraseToAnyPublisher()
-    }
-
-    var recorderStatePublisher: AnyPublisher<Bool, Never> {
-        recorderStateSubject.eraseToAnyPublisher()
-    }
-
-    var recorderDataPublisher: AnyPublisher<Data, Never> {
-        recorderDataSubject.eraseToAnyPublisher()
-    }
-
-    var normalizedFrequencyAmplitudesPublisher: AnyPublisher<[Float], Never> {
-        normalizedFrequencyAmplitudes.eraseToAnyPublisher()
-    }
-
-    var playerEnginePrgressPublisher: AnyPublisher<Double, Never> {
-        playerEnginePrgress.eraseToAnyPublisher()
-    }
-
     var isPlaying: Bool {
         get async {
             guard let player else { return false }
@@ -120,7 +83,7 @@ extension BgmAudioHelper {
     func playBgm() {
         Task {
             print(#function)
-            await startPlaying(bgmDatas[bgmState], option: .loop, volume: volume)
+            await startPlaying(bgmDatas[bgmState], volume: volume)
         }
     }
 
@@ -142,14 +105,11 @@ extension BgmAudioHelper {
     /// - Parameters:
     ///   - file: 재생할 오디오 데이터
     ///   - source: 녹음 파일/url에서 가져온 파일
-    ///   - playType: 전체 또는 부분 재생
     func startPlaying(_ file: Data?,
                       sourceType type: FileSource = .imported(.large),
-                      option: PlayType = .full,
                       volume: Float = 1.0,
                       needsWaveUpdate: Bool = false) async
     {
-        guard await checkPlayerState() else { return }
         guard let file else { return }
 
         sourceType(type)
@@ -159,38 +119,14 @@ extension BgmAudioHelper {
             await self?.stopPlaying()
         }
 
-        playerStateSubject.send((source, true))
-
-        if needsWaveUpdate {
-            updatePlayIndex()
-        }
-
-        await play(file: file, option: option, volume: volume)
+        await play(file: file, volume: volume)
     }
 
-    private func play(file: Data, option: PlayType, volume: Float) async {
-        switch option {
-        case .full:
-            do {
-                try await player?.startPlaying(data: file)
-            } catch {
-                ErrorHandler.handle(error)
-            }
-        case let .partial(time):
-            do {
-                try await player?.startPlaying(data: file)
-                try await Task.sleep(for: .seconds(time))
-                await stopPlaying()
-            } catch {
-                ErrorHandler.handle(error)
-            }
-        case .loop:
-            do {
-                try await player?.startPlaying(data: file, fade: true, isLoop: true)
-            } catch {
-                ErrorHandler.handle(error)
-            }
-        @unknown default: break
+    private func play(file: Data, volume: Float) async {
+        do {
+            try await player?.startPlaying(data: file, fade: true, isLoop: true)
+        } catch {
+            ErrorHandler.handle(error)
         }
     }
 
@@ -201,8 +137,6 @@ extension BgmAudioHelper {
 
         removePlayer()
         removeTimer()
-
-        playerStateSubject.send((source, false))
     }
 
     func pause() async {
@@ -217,28 +151,8 @@ extension BgmAudioHelper {
         }
     }
 
-    private func updatePlayIndex() {
-        cancellable = Timer.publish(every: 0.25, on: .main, in: .common)
-            .autoconnect()
-            .scan(0) { count, _ in
-                count + 1
-            }
-            .sink { [weak self] value in
-                self?.waveformUpdateSubject.send(value - 1)
-            }
-    }
-
     private func makePlayer() {
         player = ASAudioPlayer()
-    }
-
-    private func checkPlayerState() async -> Bool {
-        if await isPlaying {
-            await player?.stopPlaying()
-            removePlayer()
-            playerStateSubject.send((source, false))
-        }
-        return true
     }
 }
 
@@ -253,12 +167,6 @@ extension BgmAudioHelper {
     @discardableResult
     private func sourceType(_ type: FileSource) -> Self {
         source = type
-        return self
-    }
-
-    @discardableResult
-    func isConcurrent(_ isTrue: Bool) -> Self {
-        isConcurrent = isTrue
         return self
     }
 }
